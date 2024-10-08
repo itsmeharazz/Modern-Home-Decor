e3e9a79fb1c36ca5d313aa64b8a11374844bd4d9
[Meharaz] <[itsmeharazz@gmail.com]>
2024-10-08 23:20:40 +0600

    add seach funtion

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,22 @@
+// navbar sticky
 const navHeader = document.querySelector('.navbar');
-window.addEventListener('scroll', function(){
-let hero = document.querySelector('navbar');
-if(this.window.pageYOffset > 0){
-     
-     navHeader.classList.add('navbar-sticky')
-}else{
-     navHeader.classList.remove('navbar-sticky')
-}
+window.addEventListener('scroll', function () {
+     let hero = document.querySelector('navbar');
+     if (this.window.pageYOffset > 0) {
+
+          navHeader.classList.add('navbar-sticky')
+     } else {
+          navHeader.classList.remove('navbar-sticky')
+     }
 });
+
+// add seach infdut
+const searchBtn = document.querySelector(".seach");
+const searchBox = document.querySelector(".search-box");
+const close =document.querySelector(".fa-xmark");
+searchBtn.addEventListener("click", () => {
+     searchBox.classList.add("active");
+})
+close .addEventListener('click', ()=>{
+     searchBox.classList.remove("active");
+})
